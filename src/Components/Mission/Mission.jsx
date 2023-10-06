d07a1ee98a99aa627c932df46e5f4d09fd5a3394
Maternykh <maternyh.egor@yandex.ru>
2023-10-06 23:11:46 +0500

    first commit

--- src/Components/Mission/Mission.jsx
+++ src/Components/Mission/Mission.jsx
@@ -4,6 +4,7 @@ const Mission = () => {
   return (<div id='miss'>
     <div className='Mission'>
         <div className="mission-white-fon">
+            <img src="./9.jpg" className='miss-image'/>
             <div className="mission-title">Наша миссия</div>
             <div className="mission-desc">Мы открыли нашу первую пекарню в Саут-Энде в 2000 году с одной целью: сделать мир слаще с помощью теплых улыбок, липких липких булочек, in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidat non proident, sunt in culpa qui officia deserunt mollit anim id est Laborum.</div>
         </div>
